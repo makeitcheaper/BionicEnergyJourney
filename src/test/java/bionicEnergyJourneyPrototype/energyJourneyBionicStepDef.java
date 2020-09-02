396b40bf1693757ee7691aad78ea5df8508385cb
raviahujauk <34134515+raviahujauk@users.noreply.github.com>
2020-09-02 20:42:28 +0100

    Deleted wait import

--- src/test/java/bionicEnergyJourneyPrototype/energyJourneyBionicStepDef.java
+++ src/test/java/bionicEnergyJourneyPrototype/energyJourneyBionicStepDef.java
@@ -8,9 +8,7 @@ import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class energyJourneyBionicStepDef {
 
